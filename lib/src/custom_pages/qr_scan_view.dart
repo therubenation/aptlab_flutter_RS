2cd3712b8f579dca7a0c1ddd1f017ec317b35a0b
Ruben Steijn <therubenation@gmail.com>
2023-10-26 21:09:37 +0200

    Podfile Bugfix IOS Simulator, mobile_scanner Flutter pkg added and integrated

---
+++ lib/src/custom_pages/qr_scan_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QRScanView extends StatelessWidget {
+  const QRScanView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
